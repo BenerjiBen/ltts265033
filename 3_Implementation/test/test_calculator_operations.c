fd567094dcecf5686616490f8f6f2c6036eadd8a
BenerjiBen <63308610+BenerjiBen@users.noreply.github.com>
2021-04-16 13:05:47 +0530

    Update test_calculator_operations.c

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -1,68 +1,24 @@
-#include "unity.h"
-#include <calculator_operations.h>
-
-/* Modify these two lines according to the project */
-#include <calculator_operations.h>
-#define PROJECT_NAME    "Calculator"
-
-/* Prototypes for all the test functions */
-void test_add(void);
-void test_add_testcase2(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
-
-/* Required by the unity test framework */
+#include<stdio.h>
+#include "../unity/unity.h"
+#include "../unity/unity_internals.h"
 void setUp(){}
-/* Required by the unity test framework */
 void tearDown(){}
-
-/* Start of the application test */
-int main()
+char confirm;
+void check(void)
 {
-/* Initiate the Unity Test Framework */
-  UNITY_BEGIN();
-
-/* Run Test functions */
-  RUN_TEST(test_add);
-  RUN_TEST(test_add_testcase2);
-  RUN_TEST(test_subtract);
-  RUN_TEST(test_multiply);
-  RUN_TEST(test_divide);
-
-  /* Close the Unity Test Framework */
-  return UNITY_END();
+    TEST_ASSERT_EQUAL('y',confirm);
 }
 
-/* Write all the test functions */ 
-void test_add(void) {
-  TEST_ASSERT_EQUAL(30, add(10, 20));
-  TEST_ASSERT_EQUAL(-10, add(10, -20));
-  TEST_ASSERT_EQUAL(-30, add(-10, -20));
-  TEST_ASSERT_EQUAL(10, add(-10, 20));
-}
-void test_add_testcase2(void) {
- 
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(1500, add(750, 7500));
-}
-void test_subtract(void) {
-  TEST_ASSERT_EQUAL(-3, subtract(0, 3));
-  
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(1, subtract(1000, 900));
-}
-
-void test_multiply(void) {
-  TEST_ASSERT_EQUAL(0, multiply(1, 0));
-  
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(2, multiply(2, 5));
-}
-
-void test_divide(void) {
-  TEST_ASSERT_EQUAL(0, divide(1, 0));
-  
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(3, divide(2, 2));
+int main(){
+    FILE *fp;
+	fp=fopen("tickets_reserved.txt","r");
+     if(fp == NULL)
+    {
+        confirm='n';
+    }else{
+        confirm='y';
+    }
+    UNITY_BEGIN();
+    RUN_TEST(check);
+    return UNITY_END();
 }
