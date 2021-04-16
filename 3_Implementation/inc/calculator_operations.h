97702a6d5e5f9a6485ee42aedfe83259b3a05c24
BenerjiBen <63308610+BenerjiBen@users.noreply.github.com>
2021-04-16 13:44:48 +0530

    Update calculator_operations.h

--- 3_Implementation/inc/calculator_operations.h
+++ 3_Implementation/inc/calculator_operations.h
@@ -1,47 +1,13 @@
-/** 
-* @file calculator_operations.h
-* Calculator application with 4 mathematical operations
-*
-*/
-#ifndef __CALCULATOR_OPERATIONS_H__
-#define __CALCULATOR_OPERATIONS_H__
+/*
+int create( )
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdio_ext.h>
+int reserve(node *start)
 
-/**
-*  adds the operand1 and operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 + operand2
-*/
-int add(int operand1, int operand2);
+int cancel(int reg)
 
-/**
-*  subtracts the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 - operand2
-*/
-int subtract(int operand1, int operand2);
+void enq(node *new_node)
 
-/**
-*  multiply the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 * operand2
-*/
-int multiply(int operand1, int operand2);
+node* deq()
 
+void display()
 
-/**
-* divides the operand1 by operand2 and returns the result 
-* @param[in] operand1 
-* @param[in] operand2 
-* @return integer value of the operand1 / operand2
-* @note returns 0 for divide by 0 error
-*/
-int divide(int operand1, int operand2);
-
-#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
